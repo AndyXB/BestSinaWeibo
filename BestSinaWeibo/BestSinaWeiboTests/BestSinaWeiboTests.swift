81e99a5dfcec58599c24f94c774f927e362e9006
Xie, Andy Zhengyi <andy.zhengyi.xie@accenture.com>
2017-12-06 15:56:15 +0800

    Init project.

---
+++ BestSinaWeibo/BestSinaWeiboTests/BestSinaWeiboTests.swift
@@ -0,0 +1,36 @@
+//
+//  BestSinaWeiboTests.swift
+//  BestSinaWeiboTests
+//
+//  Created by Xie, Andy Zhengyi on 2017/12/6.
+//  Copyright © 2017年 Xie, Andy Zhengyi. All rights reserved.
+//
+
+import XCTest
+@testable import BestSinaWeibo
+
+class BestSinaWeiboTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
